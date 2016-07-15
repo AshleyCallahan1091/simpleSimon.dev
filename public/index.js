f25790a70e3e5971d27a430ba7b9d6b19e8338d1
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-07-15 09:36:09 -0500

    Fixed formatting

--- public/index.js
+++ public/index.js
@@ -6,44 +6,30 @@
 	var index = 0;
 	var allowUserInput = false;
 
-
 	function mathRandom(){
 		return Math.floor((Math.random() * 4) + 1);
 	};
 
 	function shallWePlayAGame (){
-
 		var interval = 1000;
-
 		steveSays.forEach(function (div, index){ 
-
 			setTimeout(function() {  
-				
 				$('[data-number="' + div + '"]').addClass("flash");
-				
 				setTimeout(function() { 
-					
 					$('[data-number="' + div + '"]').removeClass("flash");
 				}, interval - 100);
-
 			}, interval * index)
-
 		});
-
 		setTimeout(function() {
 			allowUserInput = true;
 		}, interval * steveSays.length)
-
 	};
-
 
 	function weArePlayingAGame (){
 
 		$('[data-info="image"]').click(function(event) { 
 		 	if (allowUserInput){
-
 				if ($(this).data("number") == steveSays[index]) { 
-				
 	    			if (steveSays.length == (index + 1)) { 
 	  					steveSays.push(mathRandom()); 
 	  					$("#score").val((index+1));
@@ -53,15 +39,13 @@
 	  				} else {
 	  					steveSays[index++];
 	  				};
-
 	  			} else {
-	    		steveSays = []; 
-	    		alert("Steve says Hail Hydra.");
-	    		$("#score").val(0);
-	    		allowUserInput = false;
-	    		index = 0;
-	    		$("button").show();
-
+		    		steveSays = []; 
+		    		alert("Steve says Hail Hydra.");
+		    		$("#score").val(0);
+		    		allowUserInput = false;
+		    		index = 0;
+		    		$("button").show();
 				}
 			};
 		});
